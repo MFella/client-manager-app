63c92d287cda0dfdaa5d64b5d721a0d55c7726d2
MFella <czarusiek112@wp.pl>
2020-05-16 14:22:39 +0200

    Basket calculation work properly

--- CarpartApp-SPA/src/app/basket/basket.component.ts
+++ CarpartApp-SPA/src/app/basket/basket.component.ts
@@ -14,33 +14,62 @@ export class BasketComponent implements OnInit {
   client: Client;
   delVal: any;
   basket: any;
+  total = 0;
+  quantsArr: number[] = [];
   constructor(public authServ: AuthService, private route: ActivatedRoute,
-    private toPrice: ToPricePipe) { }
+              private toPrice: ToPricePipe) { }
 
   ngOnInit() {
-    this.route.data.subscribe((resp) => 
+    this.route.data.subscribe((resp) =>
       {
-        //console.log(resp);
+        console.log(resp.basket);
         this.client = resp.client;
         this.basket = resp.basket;
       });
+    this.returnTotal();
+
+    for (const item of this.basket.orderItems) {
+        this.quantsArr.push(item.quantity);
+      }
+
   }
 
-  status()
-  {
+  status() {
     this.delVal = this.toPrice.transform(this.deliverValue);
-    //console.log(this.toPrice.transform(this.deliverValue));
+     console.log(this.delVal);
+     this.returnTotal();
   }
 
-  isFullDetailed()
-  {
-    
-    if(this.client.city.length < 2 || this.client.country.length < 2 || 
-        this.client.postcode.length < 2 || this.client.street.length < 2)
-        {
+  isFullDetailed() {
+
+    if (this.client.city.length < 2 || this.client.country.length < 2 ||
+        this.client.postcode.length < 2 || this.client.street.length < 2) {
           return false;
         }
     return true;
+  }
+
+  returnTotal() {
+    if (this.basket.orderItems.length !== 0) {
+      this.total = 0;
+      setTimeout(() => {
+      for (let i = 0; i < this.basket.orderItems.length; i++) {
+            console.log(this.quantsArr);
+            this.total += (this.basket.orderItems[i].product.price * this.quantsArr[i]);
+          }
+         }, 20);
+    }
+    if(this.delVal !== "" && this.delVal !== undefined)
+    {
+      this.total += parseFloat(this.delVal);
+    }
+
+  }
+
+  showChanged(i: number) {
+    console.log(this.quantsArr);
+    console.log(this.delVal);
+    this.returnTotal();
   }
 
 
