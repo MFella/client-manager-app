ec760a2857242332866633993552a10fac35d1b4
MFella <czarusiek112@wp.pl>
2020-05-29 03:49:30 +0200

    pagination of orders completed

--- CarpartApp-SPA/src/app/order-list/order-list.component.ts
+++ CarpartApp-SPA/src/app/order-list/order-list.component.ts
@@ -16,17 +16,17 @@ export class OrderListComponent implements OnInit {
   pageOfOrders: Order[] = [];
   searchTerm: string;
   filterWhat: any;
-  currPage: number;
+  currPage: number = 1;
   pag: Pagination;
 
   constructor(private custServ: CustomerService, private route: ActivatedRoute,
     public authServ: AuthService) { }
 
   ngOnInit() {
-    this.currPage = 1;
-
+    
     this.route.data.subscribe((res) => {
       this.orders = res.orders;
+      this.pageOfOrders = this.orders.slice(0, 10);
       this.filterWhat = "3";
       //delete last order, which is 'basket'
       console.log(res);
@@ -42,18 +42,22 @@ export class OrderListComponent implements OnInit {
     console.log(event);
     this.currPage = event.page;
     console.log(this.currPage)
-    this.loadOrders(this.currPage);
+    //this.loadOrders(`The page is: ${}`);
+    console.log(`the page is: ${event.page}`);
+    this.pageOfOrders = this.orders.slice((event.page-1)*event.itemsPerPage, (event.page)*event.itemsPerPage);
+    //console.log(tempOrders);
   }
 
   loadOrders(page: number)
   {
     let xd = page * 10;
-    console.log(this.orders.length);
-    for(let y = 0; y < xd -this.orders.length && y < 10; y++)
-    {
-      this.pageOfOrders[y] = this.orders[y];
-    }
-    console.log(this.pageOfOrders);
+    let tempOrders = this.orders.splice(xd, xd+10);
+    console.log(tempOrders);
+    // for(let y = 0; y < xd -this.orders.length && y < 10; y++)
+    // {
+    //   this.pageOfOrders[y] = this.orders[y];
+    // }
+    // console.log(this.pageOfOrders);
   }
 
 } 
